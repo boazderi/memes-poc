b4c8bfc29980aa17753b505f89ca512c49d65287
Boaz Deri <boazderi@gmail.com>
2023-11-29 22:37:42 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flex">
+         <div>bobo</div>
+         <div>lolo</div>
+         </div>
+    </div>
+  );
+}
+
+export default App;
